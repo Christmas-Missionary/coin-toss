a332aa372531817c7d69539f99f14806ca5b71c4
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2025-11-19 20:40:12 +0000

    debug option + ensure no standard library

--- m.sh
+++ m.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-   as main.s -o main.o -arch arm64 \
+if [[ $# -gt 0 ]] && [[ $1 == "-g" ]] ; then
+  flag="-g"
+fi
+
+   as $flag -ffreestanding -nostdlib main.s -o main.o -arch arm64 \
 && ld main.o -o toss -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64 \
 && ./toss
